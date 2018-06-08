e0f702168bcd452b7cb5a4873db1143f37ce3132
aleksandrakocic <32391316+aleksandrakocic@users.noreply.github.com>
2018-06-08 21:43:37 +0200

    working

---
+++ notifications/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+   
+    <notifications/>
+  </div>
+</template>
+
+<script>
+import notifications from './components/notifications.vue'
+
+export default {
+  name: 'app',
+  components: {
+    notifications
+  }
+}
+</script>
+
+<style>
+#app {
+color: lightblue;
+}
+</style>
---
+++ notifications/src/components/notifications.vue
@@ -0,0 +1,67 @@
+<template>
+
+  <div class='all d-flex justify-content-center'>
+
+    <ul>
+       <li>
+         <p>hello</p>
+
+         <!-- v-bind: src="" -->
+         <!-- <img src="" alt=""> -->
+       
+      </li>
+     
+      
+    </ul>
+
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'notifications',
+  props: {
+    msg: String,
+    // mozda ovo??
+    notifications: ['notifications'] 
+  },
+
+     data() {
+         return{
+    
+               info: []
+
+    }
+  },
+
+    mouted () {
+        axios
+           .get('https://api.myjson.com/bins/19wyhe')
+           .then(response => (this.axios = response.data))
+          }
+    };
+
+//     methods: {
+//            getIcon
+//     }
+// }
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.all {
+background:  darkgray;
+height: 100vh;
+}
+
+.all ul li{
+  margin-top: 20px;
+  background:black;
+
+}
+
+</style>
